81bad34779ce56789d1f441e1b488430d1263d51
jiamin.liao <jiamin.liao@zhenai.com>
2022-03-23 14:49:05 +0800

    fix

--- spring-boot-nacos-discovery/src/main/java/com/springboot/nacosdiscovery/DiscoveryController.java
+++ spring-boot-nacos-discovery/src/main/java/com/springboot/nacosdiscovery/DiscoveryController.java
@@ -1,9 +1,12 @@
 package com.springboot.nacosdiscovery;
 
+import com.alibaba.nacos.api.annotation.NacosInjected;
+import com.alibaba.nacos.api.exception.NacosException;
+import com.alibaba.nacos.api.naming.NamingService;
+import com.alibaba.nacos.api.naming.pojo.Instance;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import sun.jvm.hotspot.oops.Instance;
 
 import java.util.List;
 
